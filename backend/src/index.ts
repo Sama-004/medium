0bec13558332a91843fc504ce079a1c01b3c00bb
Sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-05-05 01:14:37 +0530

    routes and prisma schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signup", (c) => {
+  return c.text("signup route");
+});
 
-export default app
+app.post("/api/v1/signin", (c) => {
+  return c.text("singin route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("post blog");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("edit blog");
+});
+
+app.get("/api/v1/blog:id", (c) => {
+  return c.text("get blog route");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("blog bulk");
+});
+
+export default app;
